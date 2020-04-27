afdab18d1d423ac609fbf4940121f8f6d64a06c1
Bertrik Sikken <bertrik@sikken.nl>
2020-04-27 11:59:08 +0200

    Add CayennePayloadFormatTest

---
+++ cayenne/src/test/java/nl/sikken/bertrik/cayenne/CayennePayloadFormatTest.java
@@ -0,0 +1,18 @@
+package nl.sikken.bertrik.cayenne;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * See https://community.mydevices.com/t/cayenne-lpp-2-0/7510
+ */
+public final class CayennePayloadFormatTest {
+
+    @Test
+    public void testPort() {
+        Assert.assertNull(ECayennePayloadFormat.fromPort(0));
+        Assert.assertEquals(ECayennePayloadFormat.DYNAMIC_SENSOR_PAYLOAD, ECayennePayloadFormat.fromPort(1));
+        Assert.assertEquals(ECayennePayloadFormat.PACKED_SENSOR_PAYLOAD, ECayennePayloadFormat.fromPort(2));
+    }
+
+}

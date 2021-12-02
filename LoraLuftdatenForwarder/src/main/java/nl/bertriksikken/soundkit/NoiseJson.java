c804451afb42351927492bb2bef5ccef55af4701
Bertrik Sikken <bertrik@sikken.nl>
2021-12-02 23:35:31 +0100

    Fix spotbugs warning.

--- LoraLuftdatenForwarder/src/main/java/nl/bertriksikken/soundkit/NoiseJson.java
+++ LoraLuftdatenForwarder/src/main/java/nl/bertriksikken/soundkit/NoiseJson.java
@@ -1,5 +1,9 @@
 package nl.bertriksikken.soundkit;
 
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -25,6 +29,13 @@ final class NoiseJson {
         double avg = Double.NaN;
         @JsonProperty("spectrum")
         double[] spectrum = new double[0];
+
+        @Override
+        public String toString() {
+            String formattedSpectrum = Arrays.stream(spectrum).mapToObj(v -> String.format(Locale.ROOT, "%.1f", v))
+                    .collect(Collectors.joining(",", "[", "]"));
+            return String.format("{min=%.1f,max=%.1f,avg=%.1f,spectrum=%s}", min, max, avg, formattedSpectrum);
+        }
     }
 
 }

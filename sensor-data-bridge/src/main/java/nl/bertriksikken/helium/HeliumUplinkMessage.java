cbc8f59b329f12303828a4a281e6836ac3ab5f45
Bertrik Sikken <bertrik@sikken.nl>
2024-12-27 23:23:25 +0100

    Convert HeliumUplinkMessage to record.

--- sensor-data-bridge/src/main/java/nl/bertriksikken/helium/HeliumUplinkMessage.java
+++ sensor-data-bridge/src/main/java/nl/bertriksikken/helium/HeliumUplinkMessage.java
@@ -1,59 +1,48 @@
 package nl.bertriksikken.helium;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@SuppressWarnings("ArrayRecordComponent")
 @JsonIgnoreProperties(ignoreUnknown = true)
-public final class HeliumUplinkMessage {
+public record HeliumUplinkMessage(
+        @JsonProperty("app_eui") String appEui,
+        @JsonProperty("dev_eui") String devEui,
+        @JsonProperty("devaddr") String devAddr, // device address with bytes in reverse order
+        @JsonProperty("fcnt") int fcnt,
+        @JsonProperty("port") int port,
+        @JsonProperty("name") String name,
+        @JsonProperty("payload") byte[] payload,
+        @JsonProperty("reported_at") long reportedAt, // milliseconds
+        @JsonProperty("hotspots") List<HotSpot> hotSpots) {
+    public HeliumUplinkMessage {
+        appEui = Objects.requireNonNullElse(appEui, "");
+        devEui = Objects.requireNonNullElse(devEui, "");
+        devAddr = Objects.requireNonNullElse(devAddr, "");
+        name = Objects.requireNonNullElse(name, "");
+        payload = Objects.requireNonNullElse(payload.clone(), new byte[0]);
+        hotSpots = Objects.requireNonNullElse(hotSpots, new ArrayList<>());
+    }
 
-    @JsonProperty("app_eui")
-    String appEui = "";
-
-    @JsonProperty("dev_eui")
-    String devEui = "";
-
-    // device address with bytes in reverse order
-    @JsonProperty("devaddr")
-    String devAddr = "";
-
-    @JsonProperty("fcnt")
-    int fcnt;
-
-    @JsonProperty("port")
-    int port;
-
-    @JsonProperty("name")
-    String name = "";
-
-    @JsonProperty("payload")
-    byte[] payload = new byte[0];
-
-    // milliseconds
-    @JsonProperty("reported_at")
-    long reportedAt;
-
-    @JsonProperty("hotspots")
-    List<HotSpot> hotSpots = new ArrayList<>();
+    @Override
+    public byte[] payload() {
+        return payload.clone();
+    }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
-    static final class HotSpot {
-        @JsonProperty("name")
-        String name = "";
-
-        @JsonProperty("lat")
-        double latitude;
-
-        @JsonProperty("long")
-        double longitude;
-
-        @JsonProperty("rssi")
-        double rssi;
-
-        @JsonProperty("snr")
-        double snr;
+    record HotSpot(
+            @JsonProperty("name") String name,
+            @JsonProperty("lat") double latitude,
+            @JsonProperty("long") double longitude,
+            @JsonProperty("rssi") double rssi,
+            @JsonProperty("snr") double snr) {
+        HotSpot {
+            name = Objects.requireNonNullElse(name, "");
+        }
     }
 
 }
--- sensor-data-bridge/src/test/java/nl/bertriksikken/helium/HeliumUplinkMessageTest.java
+++ sensor-data-bridge/src/test/java/nl/bertriksikken/helium/HeliumUplinkMessageTest.java
@@ -18,21 +18,21 @@ public class HeliumUplinkMessageTest {
             HeliumUplinkMessage uplink = mapper.readValue(is, HeliumUplinkMessage.class);
             Assertions.assertNotNull(uplink);
 
-            Assertions.assertEquals("6081F9D16837130E", uplink.appEui);
-            Assertions.assertEquals("5A010048", uplink.devAddr);
-            Assertions.assertEquals("0004A30B001F21FA", uplink.devEui);
-            Assertions.assertEquals(0, uplink.fcnt);
-            Assertions.assertEquals("kissmapper", uplink.name);
-            Assertions.assertEquals(1, uplink.port);
-            Assertions.assertEquals(1631457565832L, uplink.reportedAt);
-            Assertions.assertArrayEquals(new byte[]{3}, uplink.payload);
+            Assertions.assertEquals("6081F9D16837130E", uplink.appEui());
+            Assertions.assertEquals("5A010048", uplink.devAddr());
+            Assertions.assertEquals("0004A30B001F21FA", uplink.devEui());
+            Assertions.assertEquals(0, uplink.fcnt());
+            Assertions.assertEquals("kissmapper", uplink.name());
+            Assertions.assertEquals(1, uplink.port());
+            Assertions.assertEquals(1631457565832L, uplink.reportedAt());
+            Assertions.assertArrayEquals(new byte[]{3}, uplink.payload());
 
-            HotSpot hotSpot = uplink.hotSpots.get(0);
-            Assertions.assertEquals(52.01745, hotSpot.latitude, 0.00001);
-            Assertions.assertEquals(4.729876, hotSpot.longitude, 0.00001);
-            Assertions.assertEquals("melted-quartz-antelope", hotSpot.name);
-            Assertions.assertEquals(-120, hotSpot.rssi, 0.1);
-            Assertions.assertEquals(-7.5, hotSpot.snr, 0.1);
+            HotSpot hotSpot = uplink.hotSpots().get(0);
+            Assertions.assertEquals(52.01745, hotSpot.latitude(), 0.00001);
+            Assertions.assertEquals(4.729876, hotSpot.longitude(), 0.00001);
+            Assertions.assertEquals("melted-quartz-antelope", hotSpot.name());
+            Assertions.assertEquals(-120, hotSpot.rssi(), 0.1);
+            Assertions.assertEquals(-7.5, hotSpot.snr(), 0.1);
         }
 
     }
